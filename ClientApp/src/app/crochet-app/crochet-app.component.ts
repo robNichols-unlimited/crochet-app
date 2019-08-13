350521134d72f4efcdf8f2debb2116f529eedc3f
Rob Nichols <rob.a.nichols@gmail.com>
2019-08-12 22:35:54 -0600

    Added images, initial pass at auto-scroll images.

--- ClientApp/src/app/crochet-app/crochet-app.component.ts
+++ ClientApp/src/app/crochet-app/crochet-app.component.ts
@@ -4,4 +4,35 @@ import { Component } from '@angular/core';
   selector: 'crochet-app',
   templateUrl: './crochet-app.component.html'
 })
-export class CrochetAppComponent { }
+export class CrochetAppComponent {
+  public slideIndex = 1;
+  showDivs(slideIndex);
+
+  showDivs(n: number) {
+    let i;
+    let x = document.getElementsByClassName("slides");
+    let dots = document.getElementsByClassName("demo");
+
+    if (n > x.length) () => this.slideIndex = 1;
+    if (n < 1) () => this.slideIndex = x.length;
+
+    for (i = 0; i < x.length; i++) {
+      x[i].className = "display-none";
+    }
+
+    for (i = 0; i < x.length; i++) {
+      dots[i].className = dots[i].className.replace("w3-white", "");
+    }
+
+    x[this.slideIndex - 1].className = "display-block";
+    dots[this.slideIndex - 1].className += " w3-white";
+  }
+
+  plusDivs(n: number) {
+    this.showDivs(this.slideIndex += n);
+  }
+
+  currentDiv(n: number) {
+    this.showDivs(this.slideIndex = n);
+  }  
+}
